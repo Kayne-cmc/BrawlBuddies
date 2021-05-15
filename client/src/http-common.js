524da35655dadcffe47a898479fdad4cdab66992
Kayne Chu <kayne.cmc@gmail.com>
2021-05-14 23:29:58 -0400

    Replaces localhost:3000 with heroku link

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   //withCredentials: true,
-  baseURL: "http://localhost:5000",
-  //baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://brawlbuddies.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*",
--- server.js
+++ server.js
@@ -20,8 +20,8 @@ app.use(express.static(path.join(__dirname, "client/build")));
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
     // origin: ["http://localhost:3000"],
+    origin: ["https://brawlbuddies.herokuapp.com/"],
     credentials: true,
 }));
 
